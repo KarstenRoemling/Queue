0e8481c2f989ebb15b51dd76beba1533c0df7024
KarstenRoemling <42942853+KarstenRoemling@users.noreply.github.com>
2019-10-03 22:15:20 +0200

    GUI update (Table and ScrollPanel added, ENTER key can be used instead of button)

--- queueBlueJ/GUI.java
+++ queueBlueJ/GUI.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.table.*;
 import java.awt.Font;
 import java.awt.event.*;
 import java.util.*;
@@ -15,9 +16,13 @@ public class GUI
     private JButton removeButton;
     private JButton addButton;
     private JTextField nameText;
+    private JScrollPane scroll;
+    private JTable table;
     
-    //ArrayList containing all JLabels (Texts) that represent elements
-    private ArrayList<JLabel> elements;
+    private GUIManager manager;
+    
+    //ArrayList containing all Strings that represent elements
+    private ArrayList<String> elements;
     
     //Queue object to read and create data structure
     private Queue queue;
@@ -27,18 +32,57 @@ public class GUI
      */
     public GUI(GUIManager pManager)
     {
+        manager = pManager;
+        
         queue = pManager.getQueue();
-        elements = new ArrayList<JLabel>();
+        elements = new ArrayList<String>();
         
         //New Window (Frame) created, window title "Warteschlange"
         frame = new JFrame("Warteschlange");
         
         //Heading Text "Warteschlange"
-        heading = new JLabel("Warteschlange - " + pManager.getName());
+        heading = new JLabel("Warteschlange - " + manager.getName());
         heading.setBounds(30,40,300,40);
         heading.setFont(new Font("Arial", Font.PLAIN, 20));
         frame.add(heading);
         
+        TableModel dataModel = new AbstractTableModel() {
+            public String getColumnName(int col) {
+                switch(col){
+                    case 0:
+                        return "Nr.";
+                    case 1:
+                        return "Name";
+                    default:
+                        return "UI-Fehler";
+                }
+            }
+            public int getColumnCount() { return 2; }
+            public int getRowCount() { return elements.size();}
+            public Object getValueAt(int row, int col) {
+                switch(col){
+                    case 0:
+                        return new Integer(row + 1);
+                    case 1:
+                        return new String(elements.get(row));
+                    default:
+                        return new String("UI-Fehler");
+                }
+            }
+        };
+        table = new JTable(dataModel);
+        table.setAutoResizeMode( JTable.AUTO_RESIZE_ALL_COLUMNS );
+        TableColumn columnNr = table.getColumn("Nr.");
+        columnNr.setMinWidth(50);
+        columnNr.setMaxWidth(50);
+        TableColumn columnName = table.getColumn("Name");
+        columnName.setMinWidth(250);
+        columnName.setMaxWidth(250);
+        
+        scroll = new JScrollPane(table);
+        scroll.setBounds(30, 100, 300, 200);
+        frame.add(scroll);
+
         //Button to dequeue the first element.
         removeButton = new JButton("Entfernen");
         removeButton.setBounds(345,45,95,30);
@@ -55,21 +99,26 @@ public class GUI
         
         //Button to enqueue a new Element.
         addButton = new JButton("Hinzufügen");
+        addButton.setBounds(345,310,95,30);
         //Click event
         addButton.addActionListener(new ActionListener(){  
             public void actionPerformed(ActionEvent e){
-                //When the Button is clicked, an new QueueElement is created and added to the Queue with the name inputted in the TextField
-                pManager.create(nameText.getText());
-                //The Text Field is made empty again
-                nameText.setText("");
-                //After that, the GUI is updated
-                update();
+                doAdd();
             }
         });
         frame.add(addButton);
         
         //A TextField (here the User can enter text) is created
         nameText = new JTextField();
+        nameText.setBounds(30,310,300,30);
+        //KeyListerner: reacts on typing enter
+        nameText.addKeyListener(new KeyAdapter(){
+            public void keyReleased(KeyEvent k){
+                if(k.getKeyCode() == KeyEvent.VK_ENTER){
+                    doAdd();
+                }
+            }
+        });
         frame.add(nameText);
         
         //The  frames size is set
@@ -85,13 +134,20 @@ public class GUI
     }
     
     /**
+     * Adds new Element to Queue and updates UI
+     */
+    private void doAdd(){
+        manager.create(nameText.getText());
+        nameText.setText("");
+        update();
+        scrollToBottom();
+    }
+    
+    /**
      * Updates the Graphical User Interface in order to represent the changed Queue object
      */
     public void update(){
         //Removes all JLabels representing an element
-        for(int i = 0; i < elements.size(); i++){
-            frame.remove(elements.get(i));
-        }
         elements.clear();
         
         QueueElement element = queue.getHead();
@@ -102,15 +158,25 @@ public class GUI
             element = element.getNachfolger();
         }
         
-        //Updates the position of the input field and the add button
-        nameText.setBounds(30,100 + elements.size() * 35,300,30);
-        addButton.setBounds(345,100 + elements.size() * 35,95,30);
+        table.updateUI();
+    }
+    
+    public void scrollToBottom(){
+        scroll.validate();
+        JScrollBar vertical = scroll.getVerticalScrollBar();
+        vertical.setValue( vertical.getMaximum() );
+    }
+    
+    public void scrollToTop(){
+        scroll.validate();
+        JScrollBar vertical = scroll.getVerticalScrollBar();
+        vertical.setValue( 0 );
     }
     
     /**
-     * Adds one QueueElement as a JLabel to the JFrame
+     * Adds one QueueElement as Text
      */
-    public void addElement(QueueElement pElement){
+    private void addElement(QueueElement pElement){
         if(pElement == null){
             return;
         }
@@ -121,11 +187,7 @@ public class GUI
             text = no.getName();
         }
         
-        JLabel elementLabel = new JLabel(text);
-        elementLabel.setBounds(30,100 + elements.size() * 35,300,30);
-        
-        elements.add(elementLabel);
-        frame.add(elementLabel);
+        elements.add(text);
     }
 
 }
